37d26d643d810cc6af5e6f5a191584e00257db49
JoanIat312 <lyt307166987@hotmail.com>
2014-07-31 11:58:56 -0700

    CutScenes added

---
+++ Final Project/Assets/Scripts/cutSceneMusic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cutSceneMusic : MonoBehaviour {
+	//public AudioClip[] bgSound;
+	// Use this for initialization
+	void Start () {
+		//AnimationEvent sound1 = new AnimationEvent ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine (loadNext());
+
+	}
+	IEnumerator loadNext(){
+		yield return new WaitForSeconds (animation.clip.length);
+		Application.LoadLevel (Application.loadedLevel + 1);
+
+	}
+	void PlaySound(int num){
+		//audio.clip = bgSound [index];
+		//audio.play();
+		Debug.Log ("play sound" + num);
+	}
+}
